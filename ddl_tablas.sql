c434a94f73dc8eb391f70a7cfa563ca33d036855
Eduardo <fcadenaszarate@gmail.com>
2019-10-26 11:50:00 -0500

    quinto cambio

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -1,10 +1,12 @@
 --creacion de tablas SQL
 --autor: Felipe Cadenas
+--fecha: 26/10/219
 --trabajando en rama Alterna - desarrollo
+--inclusion de nologging
 
 create table td_cliente (
 nombre varchar2(50),
 apellidos varchar2(50),
 direccion varchar2(100)
 )
-tablespace tmp;
+tablespace tmp nologging;
