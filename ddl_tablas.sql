4dcd2afb39d17c95cf7da7dc7d861d98b1ec6121
Eduardo <fcadenaszarate@gmail.com>
2019-10-26 11:43:53 -0500

    quinto cambio

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -1,8 +1,10 @@
 --creacion de tablas SQL
 --autor: Felipe Cadenas
+--trabajando en rama Alterna - desarrollo
 
 create table td_cliente (
 nombre varchar2(50),
 apellidos varchar2(50),
 direccion varchar2(100)
 )
+tablespace tmp;
